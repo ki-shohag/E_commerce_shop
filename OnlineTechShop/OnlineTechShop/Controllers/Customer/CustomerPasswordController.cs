80f94ed237ea2a96416c90b2f5bfff812322d26c
ki-shohag <koushikur.aiub@gmail.com>
2021-03-08 04:26:14 +0600

    implemted front end for customer login, registration, password reset along with necessary routes

---
+++ OnlineTechShop/OnlineTechShop/Controllers/Customer/CustomerPasswordController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTechShop.Controllers.Customer
+{
+    public class CustomerPasswordController : Controller
+    {
+        // GET: CustomerPassword
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult VerifyEmail()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult ResetPassword()
+        {
+            return View();
+        }
+    }
+}
