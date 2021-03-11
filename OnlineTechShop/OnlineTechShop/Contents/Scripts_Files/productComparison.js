1b4f17bec394dc8426a0e538c524006ff34f9dfe
ki-shohag <koushikur.aiub@gmail.com>
2021-03-12 04:40:35 +0600

    fixed product comparison feature and created routing and views for the nav bars

--- OnlineTechShop/OnlineTechShop/Contents/Scripts_Files/productComparison.js
+++ OnlineTechShop/OnlineTechShop/Contents/Scripts_Files/productComparison.js
@@ -32,29 +32,29 @@
 //    });
 //});
 
-$("#product1SelectBox").on("change", function () {
-    //alert(this.value);
-    mydata = JSON.stringify({ "Quantity": this.value});
-    $.ajax({
-        type: "POST",
-        url: "/Products/GetProductById",
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        data: mydata,
-        success: function (result) {
-            //alert("Successfully rated " + JSON.stringify(result) + "!");
-            mydata = JSON.parse(result);
-            $("#productTable > tbody").html("");
-            $("#productTable > tbody").append("<tr><td><b>Product Name : </b></td><td>" + mydata.ProductName + "</td></tr>");
-            $("#productTable > tbody").append("<tr><td><b>Product Category : </b></td><td>" + mydata.Category + "</td></tr>");
-            $("#productTable > tbody").append("<tr><td><b>Brand : </b></td><td>" + mydata.Brand + "</td></tr>");
-            $("#productTable > tbody").append("<tr><td><b>Description : </b></td><td>" + mydata.ProductDescription + "</td></tr>");
-            $("#productTable > tbody").append("<tr><td><b>Discount : </b></td><td>" + mydata.Discount + "</td></tr>");
-            $("#productTable > tbody").append("<tr><td><b>Tax : </b></td><td>" + mydata.Tax + "</td></tr>");
-            $("#productTable > tbody").append("<tr><td><b>Availability : </b></td><td>" + mydata.Status + "</td></tr>");
-            $("#productTable > tbody").append("<tr><td><b>Stock : </b></td><td>" + mydata.Quantity + "</td></tr>");
-            $("#productTable > tbody").append("<tr><td><b>Unit Price : </b></td><td>" + mydata.SellingPrice + "</td></tr>");
-        }
-    });
-});
+//$("#product1SelectBox").on("change", function () {
+//    //alert(this.value);
+//    mydata = JSON.stringify({ "Quantity": this.value});
+//    $.ajax({
+//        type: "POST",
+//        url: "/Products/GetProductById",
+//        contentType: "application/json; charset=utf-8",
+//        dataType: "json",
+//        data: mydata,
+//        success: function (result) {
+//            //alert("Successfully rated " + JSON.stringify(result) + "!");
+//            mydata = JSON.parse(result);
+//            $("#productTable > tbody").html("");
+//            $("#productTable > tbody").append("<tr><td><b>Product Name : </b></td><td>" + mydata.ProductName + "</td></tr>");
+//            $("#productTable > tbody").append("<tr><td><b>Product Category : </b></td><td>" + mydata.Category + "</td></tr>");
+//            $("#productTable > tbody").append("<tr><td><b>Brand : </b></td><td>" + mydata.Brand + "</td></tr>");
+//            $("#productTable > tbody").append("<tr><td><b>Description : </b></td><td>" + mydata.ProductDescription + "</td></tr>");
+//            $("#productTable > tbody").append("<tr><td><b>Discount : </b></td><td>" + mydata.Discount + "</td></tr>");
+//            $("#productTable > tbody").append("<tr><td><b>Tax : </b></td><td>" + mydata.Tax + "</td></tr>");
+//            $("#productTable > tbody").append("<tr><td><b>Availability : </b></td><td>" + mydata.Status + "</td></tr>");
+//            $("#productTable > tbody").append("<tr><td><b>Stock : </b></td><td>" + mydata.Quantity + "</td></tr>");
+//            $("#productTable > tbody").append("<tr><td><b>Unit Price : </b></td><td>" + mydata.SellingPrice + "</td></tr>");
+//        }
+//    });
+//});
 
