78aac056fc9fd3490edd391defbb2fa6fd253c09
Asif Nawaz <asifnawaz5926@gmail.com>
2021-03-13 03:28:33 +0600

    Created Basci Html Struct.

---
+++ OnlineTechShop/OnlineTechShop/Controllers/SalesExecutive/SalesExecutiveController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineTechShop.Models;
+
+namespace OnlineTechShop.Controllers.SalesExecutive
+{
+    public class SalesExecutiveController : Controller
+    {
+        private TechShopDbEntities context = new TechShopDbEntities();
+        
+        // GET: SalesExecutive
+        public ActionResult Index()
+        {
+            ViewData["SalesExecutive"] = context.SalesExecutives.ToList();
+            return View(context.SalesExecutives.ToList());
+        }
+        public ActionResult Products()
+        {
+            ViewData["SalesExecutive"] = context.SalesExecutives.ToList();
+            return View();
+        }
+    }
+}
