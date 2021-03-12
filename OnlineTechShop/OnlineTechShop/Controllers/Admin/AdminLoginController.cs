2135cd01ea916e4b011ac9b011e98856dfb239ca
SamihaNoor <samiha.nandini@gmail.com>
2021-03-13 05:16:15 +0600

    login- admin, salesExecutive, buyingAgent registration, deletion- product crud-profile_update

---
+++ OnlineTechShop/OnlineTechShop/Controllers/Admin/AdminLoginController.cs
@@ -0,0 +1,43 @@
+﻿using OnlineTechShop.Models.AdminModel.AdminDataModel;
+using OnlineTechShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTechShop.Controllers.Admin
+{
+    public class AdminLoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+        
+        [HttpPost]
+        public ActionResult Index(Models.Admin model)
+        {
+            if (ModelState.IsValid)
+            {
+                AdDataModel adminData = new AdDataModel();
+                Models.Admin admin = adminData.GetValidity(model);
+                if (admin != null)
+                {
+                    Session["userName"] = admin.UserName;
+                    Session["email"] = admin.Email;
+                    Session["id"] = admin.Id;
+                    return RedirectToAction("Index", "AdminDashboard");
+                }
+                else
+                {
+                    TempData["msg"]="Please enter your Email and Password.";
+                    return RedirectToAction("Index", "AdminLogin");
+                }
+            }
+            TempData["msg"] = "Empty";
+            return View("Login");
+        }
+    }
+}
---
+++ OnlineTechShop/OnlineTechShop/Models/AdminModel/AdminViewModel/AdminMetaData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTechShop.Models.AdminModel.AdminViewModel
+{
+    public class AdminMetaData
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required!")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "User Name is required!")]
+        public string UserName { get; set; }
+
+        public string ProfilePic { get; set; }
+
+        [Required(ErrorMessage = "Email is required!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required!")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required!")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Address is required!")]
+        public string Address { get; set; }
+
+        public System.DateTime JoiningDate { get; set; }
+        public Nullable<System.DateTime> LastUpdated { get; set; }
+    }
+}
