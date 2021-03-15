7d6842620a15fa63062160f0238faafb30f4c777
Asif Nawaz <asifnawaz5926@gmail.com>
2021-03-15 06:14:37 +0600

    Added sell or checkout

--- OnlineTechShop/OnlineTechShop/Controllers/SalesExecutive/SalesExecutiveController.cs
+++ OnlineTechShop/OnlineTechShop/Controllers/SalesExecutive/SalesExecutiveController.cs
@@ -9,6 +9,7 @@ using OnlineTechShop.Models.CustomerAccess.DataModels;
 using OnlineTechShop.Models.Sales.ViewModels;
 using OnlineTechShop.Models.Sales.DataModels;
 
+
 namespace OnlineTechShop.Controllers.SalesExecutive
 {
     public class SalesExecutiveController : Controller
@@ -52,7 +53,7 @@ namespace OnlineTechShop.Controllers.SalesExecutive
 
         public ActionResult SalesCart()
         {
-            Object obj = Session["cart"];
+            Object obj = Session["SalesCart"];
             if (obj == null)
             {
                 TempData["msg"] = "Cart is empty!";
@@ -75,17 +76,17 @@ namespace OnlineTechShop.Controllers.SalesExecutive
             if (productStockQuantity > data.Quantity)
             {
                 cart.Quantity = data.Quantity;
-                if (Session["cart"] == null)
+                if (Session["SalesCart"] == null)
                 {
                     var currentCart = cartData.GetNewCart();
                     currentCart.Add(cart);
                     totalCartQuantity = cart.Quantity;
-                    Session["cart"] = currentCart;
+                    Session["SalesCart"] = currentCart;
                     Session["cart_quantity"] = totalCartQuantity;
                 }
                 else
                 {
-                    var currentCart = (List<CartViewModel>)Session["cart"];
+                    var currentCart = (List<CartViewModel>)Session["SalesCart"];
                     var addedCart = currentCart.Where(x => x.ProductId == data.ProductId).FirstOrDefault();
                     currentCart.Remove(addedCart);
                     if (addedCart != null)
@@ -105,7 +106,7 @@ namespace OnlineTechShop.Controllers.SalesExecutive
                         currentCart.Add(cart);
                     }
                     totalCartQuantity = (int)currentCart.Sum(x => x.Quantity);
-                    Session["cart"] = currentCart;
+                    Session["SalesCart"] = currentCart;
                     Session["cart_quantity"] = totalCartQuantity;
                 }
                 return Json(JsonConvert.SerializeObject(new { msg = "Added prodcut!", totalCartQuantity = totalCartQuantity }), JsonRequestBehavior.AllowGet);
@@ -116,6 +117,59 @@ namespace OnlineTechShop.Controllers.SalesExecutive
             }
         }
 
+        [HttpGet]
+        public ActionResult RemoveProductFromCart(int id)
+        {
+            if (Session["SalesCart"] == null)
+            {
+                return RedirectToAction("SalesCart");
+            }
+            else
+            {
+                var currentCart = (List<CartViewModel>)Session["SalesCart"];
+                var deletingCart = currentCart.Where(x => x.ProductId == id).FirstOrDefault();
+                currentCart.Remove(deletingCart);
+                //int cartQuantity = (int)Session["cart_quantity"];
+                //cartQuantity -= deletingCart.Quantity;
+                //Session["cart_quantity"] = cartQuantity;
+                Session["SalesCart"] = currentCart;
+                TempData["msg"] = "Proudct is removed!";
+                return RedirectToAction("SalesCart");
+            }
+        }
+        [HttpGet]
+        public ActionResult ClearCart()
+        {
+                Session["SalesCart"] = null;
+            
+                return RedirectToAction("SalesCart");
+            
+            
+          
+        }
+        [HttpGet]
+        public ActionResult Sell()
+        {
+            Object obj = Session["SalesCart"];
+
+            List<CartViewModel> carts = (List<CartViewModel>)obj;
+            return View(carts);
+
+        }
+        [HttpPost]
+        public ActionResult Sell(FormCollection formCollection )
+        {
+            Object obj = Session["SalesCart"];
+            
+
+            if (formCollection.Get("FullName") =="" ) { TempData["fn"] = "The Name field is required"; return RedirectToAction("Sell"); }
+            else if (formCollection.Get("Address") == "") { TempData["a"] = "The Address field is required"; return RedirectToAction("Sell"); }
+            else if (formCollection.Get("Phone") == "") { TempData["p"] = "The Phone field is required"; return RedirectToAction("Sell"); }
+            List<CartViewModel> carts = (List<CartViewModel>)obj;
+
+            return RedirectToAction("Index");
+
+        }
 
     }
 }
--- OnlineTechShop/OnlineTechShop/Models/SalesExecutive.cs
+++ OnlineTechShop/OnlineTechShop/Models/SalesExecutive.cs
@@ -11,7 +11,9 @@ namespace OnlineTechShop.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
+
     public partial class SalesExecutive
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,14 +21,19 @@ namespace OnlineTechShop.Models
         {
             this.Sales_Log = new HashSet<Sales_Log>();
         }
-    
+
         public int Id { get; set; }
+      
         public string FullName { get; set; }
         public string UserName { get; set; }
         public string ProfilePic { get; set; }
+    
         public string Password { get; set; }
+        
         public string Email { get; set; }
+      
         public string Phone { get; set; }
+       
         public string Address { get; set; }
         public decimal Salary { get; set; }
         public System.DateTime JoiningDate { get; set; }
